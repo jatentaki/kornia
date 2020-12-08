8f75f42987188ccace597d621904af5e0f60642e
Michal <michal.tyszkiewicz@gmail.com>
2020-12-08 11:27:26 +0100

    Add a mask parameter to motion_from_essential_choose_solution

--- kornia/geometry/epipolar/essential.py
+++ kornia/geometry/epipolar/essential.py
@@ -1,5 +1,5 @@
 """Module containing functionalities for the Essential matrix."""
-from typing import Tuple
+from typing import Tuple, Optional
 
 import torch
 
@@ -134,7 +134,8 @@ def motion_from_essential_choose_solution(
     K1: torch.Tensor,
     K2: torch.Tensor,
     x1: torch.Tensor,
-    x2: torch.Tensor
+    x2: torch.Tensor,
+    mask: Optional[torch.Tensor] = None,
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     r"""Recovers the relative camera rotation and the translation from an estimated essential matrix.
 
@@ -151,6 +152,9 @@ def motion_from_essential_choose_solution(
           coordinates with shape :math:`(*, N, 2)`.
         x2 (torch.Tensor): The set of points seen from the first camera frame in the camera plane
           coordinates with shape :math:`(*, N, 2)`.
+        mask (torch.Tensor): A boolean mask which can be used to exclude some points from choosing
+          the best solution. This is useful for using this method with function with batched
+          sets of points of different cardinality. Mask is of shape :math:`(*, N)`.
 
     Returns:
         Tuple[torch.Tensor, torch.Tensor, torch.Tensor]: The rotation and translation plus the
@@ -163,6 +167,9 @@ def motion_from_essential_choose_solution(
     assert len(x1.shape) >= 2 and x1.shape[-1] == 2, x1.shape
     assert len(x2.shape) >= 2 and x2.shape[-1] == 2, x2.shape
     assert len(E_mat.shape[:-2]) == len(K1.shape[:-2]) == len(K2.shape[:-2])
+    if mask is not None:
+        assert len(mask.shape) >= 2, mask.shape
+        assert len(mask.shape[:-2]) == len(x1.shape[:-2]), mask.shape
 
     # compute four possible pose solutions
     Rs, ts = motion_from_essential(E_mat)
@@ -193,7 +200,10 @@ def motion_from_essential_choose_solution(
     d2 = projection.depth(R2, t2, X)
 
     # verify the point values that have a postive depth value
-    mask = ((d1 > 0.) & (d2 > 0.))
+    if mask is None:
+        mask = ((d1 > 0.) & (d2 > 0.))
+    else:
+        mask = ((d1 > 0.) & (d2 > 0.)) & mask
     mask_indices = torch.max(mask.sum(-1), dim=-1, keepdim=True)[1]
 
     # get pose and points 3d and return
